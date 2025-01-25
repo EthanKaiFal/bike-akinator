c1d861c989b4349c6e6acaf4ae2c6f7824addaf3
EthanKaiFal <ethan.kai.falconer@gmail.com>
2025-01-24 21:41:49 -0800

    pie charts are now displaying for stats pages.

--- src/compon/BrandStats.tsx
+++ src/compon/BrandStats.tsx
@@ -2,7 +2,10 @@
 import { getBrandStats, getTotalStats } from "@/app/_actions/actions"
 import { brandData, totalData } from "./interfaces"
 import { Pie, Bar, Doughnut } from 'react-chartjs-2';
+import { Chart, ArcElement, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from "chart.js"; 
 import { useEffect, useState } from "react";
+
+Chart.register(ArcElement, BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
 const BrandStats = ({brandName}: {
     brandName: string
@@ -12,7 +15,7 @@ const BrandStats = ({brandName}: {
    const [totalStat, setTotalStat] = useState<totalData | null>(null);
    const [brandStat, setBrandStat] = useState<brandData | null>(null);
    const [loading, setLoading] = useState(true);
-
+   
    
    useEffect(() => {
       getTotalStats().then((data) => {
@@ -33,69 +36,71 @@ const BrandStats = ({brandName}: {
   
     }, []);
 
-   //  const brandNum = brandStat.totalNumBikes;
-   //  const totalNumBikes = totalStat.totalNumBikes;
-   //  console.log("in here2");
-   //  const numBrokenByBrand = brandStat.numBroken;
-   //  const totalNumBroken = totalStat.totalNumBroken;
+    if (loading) return <div>Loading...</div>;
 
-   //  const numSoldByBrand = brandStat.numSold;
-   //  const totalNumSold = totalStat.totalNumSold;
+    const brandNum = brandStat?.totalNumBikes;
+    const totalNumBikes = totalStat?.totalNumBikes;
+    console.log("in here2");
+    const numBrokenByBrand = brandStat?.numBroken;
+    const totalNumBroken = totalStat?.totalNumBroken;
 
-   //  const avgSatisScoreByBrand = brandStat.avgSatisScore;
-   //  const totalAvgOwnerShip = totalStat.totalAvgOwnership;
+    const numSoldByBrand = brandStat?.numSold;
+    const totalNumSold = totalStat?.totalNumSold;
+
+    const avgSatisScoreByBrand = brandStat?.avgSatisScore;
+    const totalAvgOwnerShip = totalStat?.totalAvgOwnership;
 
    // data groups for charts
-//    const bikeData = {
-//     labels: [`${brandName} Bikes`, 'All Other Bikes'],
-//     datasets: [
-//        {
-//           data: [brandNum, totalNumBikes??0 - (brandNum??0)],
-//           backgroundColor: ['#4BC0C0', '#FF6384'],
-//        },
-//     ],
-//  };
+   const bikeData = {
+    labels: [`${brandName} Bikes`, 'All Other Bikes'],
+    datasets: [
+       {
+          data: [brandNum, totalNumBikes??0 - (brandNum??0)],
+          backgroundColor: ['#4BC0C0', '#FF6384'],
+       },
+    ],
+ };
 
-//  const satisScoreData = {
-//     labels: ['Average Satisfaction Score'],
-//     datasets: [
-//        {
-//           label: `${brandName}`,
-//           data: [avgSatisScoreByBrand],
-//           backgroundColor: '#36A2EB',
-//        },
-//        {
-//           label: 'Overall Average',
-//           data: [totalAvgOwnerShip],
-//           backgroundColor: '#FFCE56',
-//        },
+ const satisScoreData = {
+    labels: ['Average Satisfaction Score'],
+    datasets: [
+       {
+          label: `${brandName}`,
+          data: [avgSatisScoreByBrand],
+          backgroundColor: '#36A2EB',
+       },
+       {
+          label: 'Overall Average',
+          data: [totalAvgOwnerShip],
+          backgroundColor: '#FFCE56',
+       },
 
-//     ],
-//  };
+    ],
+ };
 
-//  const brokenBikesData = {
-//     labels: [`${brandName} Broken Bikes`, 'Other Broken Bikes'],
-//     datasets: [
-//        {
-//           data: [numBrokenByBrand, totalNumBroken??0 - (numBrokenByBrand??0)],
-//           backgroundColor: ['#FF9F40', '#FF6384'],
-//        },
-//     ],
-//  };
+ const brokenBikesData = {
+    labels: [`${brandName} Broken Bikes`, 'Other Broken Bikes'],
+    datasets: [
+       {
+          data: [numBrokenByBrand, totalNumBroken??0 - (numBrokenByBrand??0)],
+          backgroundColor: ['#FF9F40', '#FF6384'],
+       },
+    ],
+ };
 
-//  const soldBikesData = {
-//     labels: [`${brandName} Broken Bikes`, 'Other Broken Bikes'],
-//     datasets: [
-//        {
-//           data: [numSoldByBrand, totalNumSold??0 - (numSoldByBrand??0)],
-//           backgroundColor: ['#FF9F40', '#FF6384'],
-//        },
-//     ],
-//  };
+ const soldBikesData = {
+    labels: [`${brandName} Broken Bikes`, 'Other Broken Bikes'],
+    datasets: [
+       {
+          data: [numSoldByBrand, totalNumSold??0 - (numSoldByBrand??0)],
+          backgroundColor: ['#FF9F40', '#FF6384'],
+       },
+    ],
+ };
  console.log("in here");
     return (
         <div className="chart-container">
-        {/* <div className="chart-item">
+        <div className="chart-item">
            <h3>Bikes Distribution</h3>
            <Pie data={bikeData} />
         </div>
@@ -110,8 +115,7 @@ const BrandStats = ({brandName}: {
         <div className="chart-item">
             <h3> Sold Bikes</h3>
             <Doughnut data={soldBikesData}/>
-        </div> */}
-        inside
+        </div>
      </div>
     )
 }
