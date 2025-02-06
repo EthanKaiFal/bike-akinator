f031eef4b24275e2270a965c5077b414eb3850f4
EthanKaiFal <ethan.kai.falconer@gmail.com>
2025-02-05 17:41:49 -0800

    home page
    Made it very slightly more inviting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,12 @@ import { cookieBasedClient, isAuthenticated } from "@/utils/amplify-utils";
 
 
 export default async function Home() {
-  var posts: any[] = [];
   try {
-    if (await isAuthenticated()) {
-      const { data: post, errors } = await cookieBasedClient.models.Post.list({
-        selectionSet: ["title", "id"],
 
-
-      })
-      posts = post;
-      if (errors) {
-        console.log("home has errors");
-      }
-    }
-    else {
-    }
-    console.log("here 8");
 
     return (
       <main className="flex flex-col items-center justify-between p-24">
-        <h1 className="text-2xl pb-10">List Of ALL Titles</h1>
+        <h1 className="text-2xl pb-10">Welcome to Bike Akinator!!!!</h1>
       </main>
 
     );
